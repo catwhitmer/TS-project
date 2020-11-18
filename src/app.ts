9ca61a6b05f48a2fbe7f8a21752d90ecd7b479e5
catwhitmer <catwhitmer@gmail.com>
2020-11-18 13:01:59 -0500

    gained access to the input elements

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -16,8 +19,20 @@ class ProjectInput {
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
     this.attach();
   }
+
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
