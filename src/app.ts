0d4f07ad0be98d68abd0ce1cdeafee9cbbebd9ef
catwhitmer <catwhitmer@gmail.com>
2020-11-18 15:32:24 -0500

    added a base class

--- src/app.ts
+++ src/app.ts
@@ -114,6 +114,33 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+// Component Base Class
+class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    newElementId?: string
+  ) {
+    this.templateElement = document.getElementById(
+      templateId
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as U;
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+  }
+}
+
 // ProjectList Class
 class ProjectList {
   templateElement: HTMLTemplateElement;
