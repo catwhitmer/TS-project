6df1ffb8fa57d4eb901725b243aaba5f2196f220
catwhitmer <catwhitmer@gmail.com>
2020-11-18 12:49:35 -0500

    created Project Input class with constructor and html elements

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+// ProjectInput Class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+  }
+}
