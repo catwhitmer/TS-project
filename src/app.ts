c95c1a8bf18b7b4e97940b59e52d136703765a79
catwhitmer <catwhitmer@gmail.com>
2020-11-18 15:23:01 -0500

    fixed the duplication issue

--- src/app.ts
+++ src/app.ts
@@ -154,6 +154,7 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`
     )! as HTMLUListElement;
+    listEl.innerHTML = "";
     for (const projItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = projItem.title;
